fe30234375aba1dc40093c7d079328ac155deafe
IrisWang <39836651+iriswhhh@users.noreply.github.com>
2021-10-19 10:02:28 -0700

    Add tests for READ and UPDATE

--- tests/test_app.py
+++ tests/test_app.py
@@ -36,3 +36,87 @@ def test_create_incorrect_data_type():
     assert response.status_code == 400
     assert response.is_json
     assert 'error' in response.json
+
+    
+def test_read_correct():
+    test_app.put('/supplier',
+        data=json.dumps({
+            'name': 'Tom',
+            'email': 'Tom@gmail.com',
+            'address': 'New York',
+            'products': [1, 2, 3]
+            }), content_type='application/json')
+    response = test.get('/supplier/1')
+    assert response is not None
+    assert response.is_json
+    assert response.status_code == 200
+
+def test_read_correct_data():
+    test_app.put('/supplier',
+        data=json.dumps({
+            'name': 'Tom',
+            'email': 'Tom@gmail.com',
+            'address': 'New York',
+            'products': [1, 2, 3]
+            }), content_type='application/json')
+    response = test_app.get('/supplier/1')
+    assert response is not None
+    assert response.is_json
+    assert response.status_code == 200
+    assert 'id' in response.json
+    assert response.json['id'] == 1
+
+def test_read_not_found():
+    response = test_app.get('/supplier/10000')
+    assert response is not None
+    assert response.is_json
+    assert response.status_code == 404
+    assert 'error' in response.json
+
+def test_read_correct():
+    test_app.put('/supplier',
+        data=json.dumps({
+            'name': 'Tom',
+            'email': 'Tom@gmail.com',
+            'address': 'New York',
+            'products': [1, 2, 3]
+            }), content_type='application/json')
+    response = test_app.get('/supplier/1')
+    assert response is not None
+    assert response.is_json
+    assert response.status_code == 200
+    assert 'id' in response.json
+    assert response.json['id'] == 1
+
+def test_update_not_found():
+    response = test_app.post('/supplier/10000')
+    assert response is not None
+    assert response.is_json
+    assert response.status_code == 404
+    assert 'error' in response.json
+
+def test_update_and_read_correct():
+    test_app.put('/supplier',
+        data=json.dumps({
+            'name': 'Tom',
+            'email': 'Tom@gmail.com',
+            'address': 'New York',
+            'products': [1, 2, 3]
+            }), content_type='application/json')
+    test_app.post('/supplier/1',
+        data=json.dumps({
+            'name': 'update1',
+            'email': 'update1@gmail.com',
+            'address': 'NYU',
+            'products': [2]
+            }), content_type='application/json')
+    response = test_app.get('/supplier/1')
+    assert response is not None
+    assert response.is_json
+    assert response.status_code == 200
+    assert 'id' in response.json
+    assert response.json['id'] == 1
+    assert response.json['name'] == 'update1'
+    assert response.json['email'] == 'update1@gmail.com'
+    assert response.json['address'] == 'NYU'
+    assert response.json['products'] == [2]
