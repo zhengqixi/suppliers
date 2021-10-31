c42ce6e6a21e5e87dab94104caa5e4377a97f826
IrisWang <39836651+iriswhhh@users.noreply.github.com>
2021-10-30 21:25:30 -0700

    Update error_handler.py
    Update error_handler.py to handle HTTP_404_NOT_FOUND

--- service/error_handlers.py
+++ service/error_handlers.py
@@ -2,6 +2,9 @@ from flask import jsonify
 from . import app, status
 
 
+######################################################################
+# Error Handlers
+######################################################################
 @app.errorhandler(status.HTTP_400_BAD_REQUEST)
 def bad_request(error):
     """Handles bad reuests with 400_BAD_REQUEST"""
@@ -10,4 +13,15 @@ def bad_request(error):
     return (
         jsonify(status=status.HTTP_400_BAD_REQUEST, error=message),
         status.HTTP_400_BAD_REQUEST,
+    )
+
+
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """Handles resources not found with 404_NOT_FOUND"""
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(status=status.HTTP_404_NOT_FOUND, error=message),
+        status.HTTP_404_NOT_FOUND,
     )
