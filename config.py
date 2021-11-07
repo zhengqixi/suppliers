f06eccd7f3297e25cb7fc5c5f1f670517c4841b4
A Ji <ja3802@nyu.edu>
2021-11-07 18:29:05 -0500

    add cloud foundry support

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ import logging
 # Get configuration from environment
 DATABASE_URI = os.getenv(
     "DATABASE_URI",
-    "postgres://postgres:postgres@localhost:5432/postgres"
+    "postgres://aplcyqcc:QRk6pnQJsfYwaCGnFf-Pgk8SUdzL5Wck@fanny.db.elephantsql.com/aplcyqcc"
 )
 
 # override if we are running in Cloud Foundry
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.environ['VCAP_SERVICES'])
-    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+# if 'VCAP_SERVICES' in os.environ:
+#     vcap = json.loads(os.environ['VCAP_SERVICES'])
+#     DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
