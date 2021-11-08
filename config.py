2fae934585953298ad611642f7fd329fd0166857
A Ji <ja3802@nyu.edu>
2021-11-07 19:00:16 -0500

    fix environment

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ import logging
 # Get configuration from environment
 DATABASE_URI = os.getenv(
     "DATABASE_URI",
-    "postgres://aplcyqcc:QRk6pnQJsfYwaCGnFf-Pgk8SUdzL5Wck@fanny.db.elephantsql.com/aplcyqcc"
+    "postgres://postgres:postgres@localhost:5432/postgres"
 )
 
 # override if we are running in Cloud Foundry
